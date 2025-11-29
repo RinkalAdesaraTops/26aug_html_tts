b13a7a70ef5cbd2fd334eaafef3b09599186a90f
Rinkal <rinkaladesara.tops@gmail.com>
2025-11-29 09:21:35 +0530

    js

---
+++ javascript/assign.js
@@ -0,0 +1,23 @@
+// +=,-=,*=,/=,%=,**=
+let a=10
+document.write('<br>A is '+a)
+a += 5 //a=a+5
+document.write('<br>A is '+a)
+a **=2
+document.write('<br>A is '+a)
+a%=15
+document.write('<br>A is '+a)
+// relational operator -- <,>,<=,>=,==,!=,===,!==
+a=25
+b="25"
+// let b = parseInt(prompt("Enter b:"))
+document.write('<br>a > b is '+(a>b))
+document.write('<br>a < b is '+(a<b))
+document.write('<br>a >= b is '+(a>=b)) //25>=25
+document.write('<br>a <= b is '+(a<=b))
+document.write('<br>a == b is '+(a==b)) //check only values
+document.write('<br>a != b is '+(a!=b))
+document.write('<br>a === b is '+(a===b)) //check value & datatype both 
+document.write('<br>a !== b is '+(a!==b))
+
+
